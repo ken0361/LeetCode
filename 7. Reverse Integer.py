ae45ad28da2973bcd1742d2f3dde40e340925ed8
ken0361 <58049481+ken0361@users.noreply.github.com>
2020-01-05 19:34:34 +0000

    Add files via upload

---
+++ 7. Reverse Integer.py
@@ -0,0 +1,13 @@
+def reverse(x):
+
+    if x > 0:
+        x = int(str(x)[::-1])
+    else:
+        x = int(str(abs(x))[::-1])*(-1)
+    
+    if x<2**31 and x>=(-1)*(2**31):
+        return x
+    else:
+        return 0
+
+print(reverse(1534236469))
